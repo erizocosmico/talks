166c34281b2f7ef9c8b1d7508eda7f6eb0d61316
Miguel Molina <hi@mvader.me>
2016-05-16 19:43:30 +0200

    Missing things

--- intro-go/code/maps.go
+++ intro-go/code/maps.go
@@ -4,10 +4,11 @@ meaningOfLife := myMap["meaning of life"]
 
 // we can check if the map exists by requesting a second value
 meaningOfLife, ok := myMap["meaning of life"]
+delete(myMap, "meaning of life")
 
 var uninitializedMap map[string]int
 // This will crash because it is not initialized
-uninitializedMap["foo"] = 42 //hl
+uninitializedMap["foo"] = 42
 
 // We can also initialize inline
 otherMap := map[rune]bool{'F': false}
